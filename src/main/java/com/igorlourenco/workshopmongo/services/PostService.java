17a120f1353b37b0ba6650dd4ecef07442afb5bc
igor-lourenco <igor.lourencosantos@gmail.com>
2020-10-08 15:14:18 -0300

    Consulta simples com query

--- src/main/java/com/igorlourenco/workshopmongo/services/PostService.java
+++ src/main/java/com/igorlourenco/workshopmongo/services/PostService.java
@@ -1,10 +1,11 @@
 package com.igorlourenco.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.igorlourenco.workshopmongo.domain.Post;
-import com.igorlourenco.workshopmongo.domain.Usuario;
 import com.igorlourenco.workshopmongo.repository.PostRepository;
 import com.igorlourenco.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -17,12 +18,16 @@ public class PostService {
 
 	
 	public Post buscarPorId(String id) {
-		Post usuario = repo.find();
+		Post usuario = repo.findOne();
 		if(usuario == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		
 		return usuario;
 	}
-
+	
+	public List<Post> busarPorTitulo(String texto){
+		return repo.findByTtileContainingIgnoreCase(texto);
+	}
+ 
 }
